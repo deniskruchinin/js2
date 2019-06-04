28fdf0bc5824c98f4dad64b7e93358fe2d5757e1
deniskruchinin <mega-tresh@mail.ru>
2019-06-04 13:36:56 +0300

    2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,80 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 30},
-    {id: 3, title: 'Keyboard', price: 55},
-    {id: 4, title: 'Gamepad', price: 65},
-    {id: 5},
-];
+class ProductsList {
+    constructor(container = '.products'){
+        this.data = [];
+        this.container = container;
+        this.productsAll = [];
+        this.init();
+    }
+    init(){
+        this._fetchProducts();
+        this._render();
+    }
+    _fetchProducts(){
+        this.data = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 30},
+            {id: 3, title: 'Keyboard', price: 55},
+            {id: 4, title: 'Gamepad', price: 65},
+            {id: 5, title: 'Chair', price: 120, template: 1},
+        ];
+    }
+    _render(){
+        const block = document.querySelector(this.container);
+        for (let product of this.data){
+            const prod = new ProductItem(product);
+            this.productsAll.push(prod);
+            block.insertAdjacentHTML('beforeend', prod.render());
+        }
+    }
+}
 
-const renderProduct = (title="укажите товар", price="укажите цену") => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
 
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productsList.join(" ");
-};
+class ProductItem {
+    constructor(product, img = `https://placehold.it/200x150`) {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img
+    }
 
-renderPage(products);
+    render(){
+        return `<div class="product-item">
+                  <img src="${this.img}" alt="${this.title}">
+                  <div class="desc">
+                      <h3>${this.title}</h3>
+                      <p>${this.price}</p>
+                      <button class="buy-btn">Купить</button>
+                  </div>
+              </div>`
+    }
+
+}
+const products = new ProductsList();
+
+
+// /*
+//
+// //class Cart  {
+//     constructor(){
+// cartData[]// количество товаров в корзине
+//     }
+//     this.cartData(){}// метод добавления товара в корзину
+//     calc(){} // подсчет общей стоимости товаров
+//
+// }
+//
+class CartItem extends  ProductItem{
+    render() {
+        return `<div class="product-item">
+                  <img src="${this.img}" alt="${this.title}">
+                  <div class="desc">
+                      <h3>${this.title}</h3>
+                      <p>${this.price}</p>
+                      <button class="buy-btn">Купить</button>
+                      <button class="buy-btn">Удалить</button>
+                  </div>
+              </div>`
+    }
+
+}
+
